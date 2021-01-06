e9a9a153419a2b375c4d31e741ec638d95d7301c
ADiks <antondikson@gmail.com>
2021-01-06 18:58:45 +0200

    lazy load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,36 @@
-import React, { FC } from 'react'
+import React, { FC, lazy, Suspense } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { AuthPageWrapper } from './containers/auth/AuthPageWrapper'
-import { LogInForm } from './pages/login/Login'
-import { HomePage } from './pages/home/HomePage'
-import { Sign } from './pages/sign/Sign'
-import { NotFound } from './pages/not-found/NotFound'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const Home = lazy(() => import('./pages/home/HomePage'))
+const Sign = lazy(() => import('./pages/sign/Sign'))
+const Login = lazy(() => import('./pages/login/Login'))
+const NotFound = lazy(() => import('./pages/not-found/NotFound'))
 
 export const App: FC = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/home" exact>
-          <HomePage />
-        </Route>
-        <Route path="/sign" exact>
-          <AuthPageWrapper title="Sign to Twitter">
-            <Sign />
-          </AuthPageWrapper>
-        </Route>
-        <Route path="/login" exact>
-          <AuthPageWrapper title="Log in to Twitter">
-            <LogInForm />
-          </AuthPageWrapper>
-        </Route>
-        <Route>
-          <NotFound/>
-        </Route>
-      </Switch>
+      <Suspense fallback={<CircularProgress />}>
+        <Switch>
+          <Route path="/home" exact>
+            <Home />
+          </Route>
+          <Route path="/sign" exact>
+            <AuthPageWrapper title="Sign to Twitter">
+              <Sign />
+            </AuthPageWrapper>
+          </Route>
+          <Route path="/login" exact>
+            <AuthPageWrapper title="Log in to Twitter">
+              <Login />
+            </AuthPageWrapper>
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   )
 }
